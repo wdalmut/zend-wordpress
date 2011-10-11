fd25e5bc18ba08ee8c854dcba1b199a223acae8f
Walter Dal Mut <walter.dalmut@gmail.com>
2011-10-11 20:43:42 +0200

    Comments

--- src/Wally/Wordpress/Tool.php
+++ src/Wally/Wordpress/Tool.php
@@ -1,6 +1,17 @@
 <?php 
+/**
+ * Class used for 5.x porting
+ *
+ * @author Walter Dal Mut
+ */ 
 class Wally_Wordpress_Tool
 {
+    /**
+     * Lower case the first character of a string
+     *
+     * @param string $string The string to filter
+     * @return string The string with the first character lowered
+     */
 	public static function lcfirst($string)
 	{
 		return strtolower($string{0}) . substr($string, 1);
