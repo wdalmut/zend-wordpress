cbfadf4048069c37afe476e1817b09dbdb3b2c3b
Walter Dal Mut <walter.dalmut@gmail.com>
2011-10-12 14:41:25 +0200

    Finder chains

--- src/Wally/Wordpress/Model/ArrayStruct.php
+++ src/Wally/Wordpress/Model/ArrayStruct.php
@@ -13,37 +13,63 @@
 class Wally_Wordpress_Model_ArrayStruct
     extends Wally_Wordpress_Model_ArrayAccessAbstract
 {
-    public function find($name, $arg, $all = false, $operator = self::EQUALS)
-    {
-        $elements = array();
-        foreach ($this->container as $element) {
-    
-            $status = false;
-            switch ($operator) {
-                case Wally_Wordpress_Model_ArrayAccessAbstract::EQUALS:
-                    $status = $element->$name == $arg;
-                    break;
-                case Wally_Wordpress_Model_ArrayAccessAbstract::GREATER_THAN:
-                    $status = $element->$name > $arg;
-                    break;
-                case Wally_Wordpress_Model_ArrayAccessAbstract::LESS_THAN:
-                    $status = $element->$name < $arg;
-                    break;
-            }
-    
-            if ($status) {
-                if (!$all) {
-                    return $element;
-                } else {
-                    $elements[] = $element;
-                }
-            }
-        }
-    
-        if (count($elements)) {
-            return $elements;
-        } else {
-            return false;
-        }
+	public function find($name, $arg, $all = false, $operator = self::EQUALS)
+	{
+		if ($arg instanceof Zend_Date) {
+			$elements = new self();
+			foreach ($this->container as $element) {
+				$status = false;
+				switch ($operator) {
+					case Wally_Wordpress_Model_ArrayAccessAbstract::EQUALS:
+						$status = $element->$name->equals($arg);
+						break;
+					case Wally_Wordpress_Model_ArrayAccessAbstract::GREATER_THAN:
+						$status = $element->$name->isLater($arg);
+						break;
+					case Wally_Wordpress_Model_ArrayAccessAbstract::LESS_THAN:
+						$status = $element->$name->isEarlier($arg);
+						break;
+				}
+				
+				if ($status) {
+					if (!$all) {
+						return $element;
+					} else {
+						$elements[] = $element;
+					}
+				}
+			}
+		} else {
+			$elements = new self();
+			foreach ($this->container as $element) {
+				
+				$status = false;
+				switch ($operator) {
+					case Wally_Wordpress_Model_ArrayAccessAbstract::EQUALS:
+						$status = $element->$name == $arg;
+						break;
+					case Wally_Wordpress_Model_ArrayAccessAbstract::GREATER_THAN:
+						$status = $element->$name > $arg;
+						break;
+					case Wally_Wordpress_Model_ArrayAccessAbstract::LESS_THAN:
+						$status = $element->$name < $arg;
+						break;
+				}
+				
+				if ($status) {
+					if (!$all) {
+						return $element;
+					} else {
+						$elements[] = $element;
+					}
+				}
+			}
+		}
+		
+		if (count($elements)) {
+		    return $elements;
+		} else {
+		    return false;
+		}
     }
 }
