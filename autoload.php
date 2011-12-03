9f959f5001990c2e2dd9ef6d1dc1cb6da56a6e2e
Walter Dal Mut <walter.dalmut@gmail.com>
2011-12-03 11:31:31 +0100

    base phar lib

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php 
+set_include_path(
+    implode(DIRECTORY_SEPARATOR, array(
+        realpath(dirname(__FILE__)) . '/src',
+        get_include_path()
+    ))
+);
+
+//No autoload... ZF1 git missing...
+require_once ('Wally/Compile.php');
